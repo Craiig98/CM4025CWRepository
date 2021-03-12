763fd82cb3e4ab66162dc48a0bbb37ec6d43ece2
Craiig98 <craigleith98@gmail.com>
2021-03-12 11:35:10 +0000

    Partial front end completed. Basic home page running with image displayed. Moving onto full front end next

--- server/express.js
+++ server/express.js
@@ -10,6 +10,16 @@ import userRoutes from './routes/user.routes'
 import authRoutes from './routes/auth.routes'
 //comment out before building for production
 import devBundle from './devBundle'
+// modules for server side rendering
+import React from 'react'
+import ReactDOMServer from 'react-dom/server'
+import MainRouter from './../client/MainRouter'
+import { StaticRouter } from 'react-router-dom'
+
+import { ServerStyleSheets, ThemeProvider } from '@material-ui/styles'
+import theme from './../client/theme'
+//end
+
 
 
 const app = express()
@@ -31,8 +41,26 @@ app.use(helmet())
 app.use(cors())
 // template.js file
 app.get('/', (req, res) => {
-    res.status(200).send(Template())
-    })
+    const sheets = new ServerStyleSheets()
+    const context = {}
+    const markup = ReactDOMServer.renderToString(
+    sheets.collect(
+        <StaticRouter location={req.url} context={context}>
+        <ThemeProvider theme={theme}>
+        <MainRouter />
+        </ThemeProvider>
+        </StaticRouter>
+    )
+    )
+    if (context.url) {
+        return res.redirect(303, context.url)
+    }
+    const css = sheets.toString()
+    res.status(200).send(Template({
+        markup: markup,
+        css: css
+     }))
+})
     
 app.use('/', userRoutes)
 app.use('/', authRoutes)
