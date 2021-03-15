571d65bea555286193a9181fdac723046476f4dc
Craiig98 <craigleith98@gmail.com>
2021-03-15 16:28:26 +0000

    server.js now using enviroment variables/.bat file to connect to mongoDB

--- server/myBatchFile.bat
+++
@@ -1,3 +0,0 @@
-set PORT=3000 && set JWT_SECRET="mysecret" &&
-set MONGODB_URI="mongodb+srv://MyDBAdmin:<password>@ewslab4.zh61e.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
-npm run-script development
--- server/server.js
+++ server/server.js
@@ -3,11 +3,11 @@ import app from './express'
 import mongoose from 'mongoose'
 
 
-const uri = "mongodb+srv://MyDBAdmin:I3nmOKU2Ign3a7te@cluster0.zh61e.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+//const uri = "mongodb+srv://MyDBAdmin:I3nmOKU2Ign3a7te@cluster0.zh61e.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 
 // Connection URL
 mongoose.Promise = global.Promise
-mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true,
+mongoose.connect(config.mongoUri.substring(1, config.mongoUri.length - 1), { useNewUrlParser: true, useCreateIndex: true,
 useUnifiedTopology: true, dbName: "myFirstDatabase" })
 mongoose.connection.on('error', () => {
  throw new Error(`unable to connect to database: ${config.mongoUri}`)
